eff8a51e56dcd4318175e60355a4f7420a4ace25
ReedmByford <reedbyford@gmail.com>
2022-04-13 02:25:38 -0500

    add codeWar for 04-13-2022

--- Apr2022/04-13-2022/script.js
+++ Apr2022/04-13-2022/script.js
@@ -4,7 +4,7 @@
 
 // You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
 
-// For example:
+// For example :
 
 // time = 3 ----> litres = 1
 
