6510c877dc0c3daa6eaa9710cab778c5216f8c59
reedbyford <reedbyford@gmail.com>
2022-04-30 04:35:47 -0500

    Calculate average

--- Apr2022/04-30-2022/script.js
+++ Apr2022/04-30-2022/script.js
@@ -13,4 +13,8 @@ function find_average(array) {
       }
       return additionTotal / howManyNums;
     }
-  }
+}
+
+var find_average = (array) => {  
+  return array.length === 0 ? 0 : array.reduce((acc, ind)=> acc + ind, 0)/array.length
+}
