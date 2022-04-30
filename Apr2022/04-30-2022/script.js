b22776aa3a57a66c72956c939588e663752413e9
reedbyford <reedbyford@gmail.com>
2022-04-30 01:49:09 -0500

    Calculate average

---
+++ Apr2022/04-30-2022/script.js
@@ -0,0 +1,16 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+function find_average(array) {
+    if(array.length === 0){
+      return 0;
+    }else{
+      let howManyNums = 0
+      let additionTotal = array.reduce((acc,c) => acc + c, 0);
+      for(let i = 0; i < array.length; i++){
+        howManyNums = howManyNums + 1;
+      }
+      return additionTotal / howManyNums;
+    }
+  }
