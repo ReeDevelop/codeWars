706de5af5d7510caf73222c9c502a14c6128ef0c
reedbyford <reedbyford@gmail.com>
2022-05-22 00:04:28 -0500

    Get Planet Name By ID

---
+++ May2022/05-20-2022/script.js
@@ -0,0 +1,26 @@
+/* The function is not returning the correct values. Can you figure out why?
+
+Example (Input --> Output ):
+
+3 --> "Earth" */
+
+function getPlanetName(id){
+    switch(id){
+      case 1:
+        return id = 'Mercury'
+      case 2:
+        return id = 'Venus'
+      case 3:
+        return id = 'Earth'
+      case 4:
+        return id = 'Mars'
+      case 5:
+        return id = 'Jupiter'
+      case 6:
+        return id = 'Saturn'
+      case 7:
+        return id = 'Uranus'
+      case 8:
+        return id = 'Neptune'
+    }
+}
