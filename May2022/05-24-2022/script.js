200d533c979559cb66960ff858cb64653f1d35da
ReedmByford <reedbyford@gmail.com>
2022-05-24 12:36:09 -0500

    Exes and Ohs

--- May2022/05-24-2022/script.js
+++ May2022/05-24-2022/script.js
@@ -25,3 +25,9 @@ function XO(str) {
       return false;
     }
 }
+
+function XO(str) {
+    let x = str.match(/x/gi);
+    let o = str.match(/o/gi);
+    return (x && x.length) === (o && o.length);
+}
