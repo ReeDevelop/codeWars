e4db61ec9358cd0c133be046ec556713972b4cd1
reedbyford <reedbyford@gmail.com>
2022-06-29 21:47:08 -0500

    Anagram Detection

---
+++ Jun2022/06-29-2022/script.js
@@ -0,0 +1,30 @@
+/* An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+Note: anagrams are case insensitive
+
+Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+Examples
+
+    "foefet" is an anagram of "toffee"
+
+    "Buckethead" is an anagram of "DeathCubeK" */
+
+
+
+var isAnagram = function(test, original){
+    let newTest = test.toLowerCase().split('').sort()
+    let newOrig = original.toLowerCase().split('').sort()
+    function arrayEquals(a, b) {
+      return Array.isArray(a) &&
+          Array.isArray(b) &&
+          a.length === b.length &&
+          a.every((val, index) => val === b[index]);
+    }
+    return arrayEquals(newTest, newOrig);
+};
+
+var isAnagram = function(test, original) {
+    var t = test.toLowerCase().split('').sort().join('');
+    var o = original.toLowerCase().split('').sort().join('');
+    return (t==o)?true:false;
+};
