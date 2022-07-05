c09c5f8e2851348718d188b998c1ef2ff317fcbf
reedbyford <reedbyford@gmail.com>
2022-07-04 21:46:44 -0500

    Count the divisors of a number

--- July2022/07-04-2022/script.js
+++ July2022/07-04-2022/script.js
@@ -17,3 +17,11 @@ function getDivisorsCnt(n){
     }
     return divisors.length;
 }
+
+function getDivisorsCnt(n) {
+    for (var d = 0, i = n; i > 0; i--) {
+      if (n % i == 0) d++;
+    }
+    
+    return d;
+}
